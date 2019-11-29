edeba2ad95fd8be238b68cc7a30987f94f834b70
unknown <bruno152251281@gmail.com>
2019-11-29 17:42:45 +0000

    lol

---
+++ Projeto/code/lattice_square.py
@@ -0,0 +1,28 @@
+import graph
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from math import log
+
+class Lattice_Square(graph.Graph):
+
+    def __init__(self, size):
+        self.size = size
+
+    def build(self):
+        chumbo = self.size*self.size
+        print(chumbo)
+        '''for id in range(chumbo):
+            if id not in self.graph:
+                self.graph[id] = []
+                self.degree[id] = 0'''
+        for id in range(chumbo):
+            if id%self.size != 9:
+                self.addEdge(id, id+1)
+            self.addEdge(id, id + self.size)
+            #self.addEdge(id, id-1)
+            #self.addEdge(id, id - self.size)
+
+x = Lattice_Square(10)
+x.build()
+x.saveGraphToFile("lattice.edges")
